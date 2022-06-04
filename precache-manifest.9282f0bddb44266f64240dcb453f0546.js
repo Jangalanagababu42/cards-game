74ab952f5ac0bb08394baea76874faac566c1f9c
jangalanagababu42 <jangalanagababu42@gmail.com>
2022-06-04 11:21:47 +0530

    Updates

---
+++ precache-manifest.9282f0bddb44266f64240dcb453f0546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7333a05dcb7863554f4b",
+    "url": "/cards-game/static/css/main.d8123af8.chunk.css"
+  },
+  {
+    "revision": "7333a05dcb7863554f4b",
+    "url": "/cards-game/static/js/main.3499047c.chunk.js"
+  },
+  {
+    "revision": "136644fbced356de9524",
+    "url": "/cards-game/static/js/runtime~main.7f9822b8.js"
+  },
+  {
+    "revision": "29234b97bbd5055a1257",
+    "url": "/cards-game/static/js/2.3517c206.chunk.js"
+  },
+  {
+    "revision": "d20fe06b29f94800ebde2fa0855fb784",
+    "url": "/cards-game/index.html"
+  }
+];
